3a2f1424cef896cd4c814f8fae993ce5e399fde5
Jirka <jirka.borovec@seznam.cz>
2023-02-13 15:31:12 +0100

    simplify setup

--- setup.py
+++ setup.py
@@ -1,41 +1,28 @@
 #!/usr/bin/env python
 import os
-from typing import List
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
+_PATH_ROOT = os.path.dirname(__file__)
 
-def _load_requirements(path_dir: str, file_name: str = "requirements.txt", comment_char: str = "#") -> List[str]:
-    """Load requirements from a file."""
-    with open(os.path.join(path_dir, file_name)) as file:
-        lines = [ln.strip() for ln in file.readlines()]
-    reqs = []
-    for ln in lines:
-        # filer all comments
-        if comment_char in ln:
-            ln = ln[: ln.index(comment_char)].strip()
-        # skip directly installed dependencies
-        if ln.startswith("http"):
-            continue
-        # skip index url
-        if ln.startswith("--extra-index-url"):
-            continue
-        if ln:  # if requirement is not empty
-            reqs.append(ln)
-    return reqs
+
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
 
 
 setup(
-    name="lightning_deepchecks",
+    name="lightning-deepchecks",
     version="0.0.1",
     description="⚡ Deepchecks for Lightning ⚡",
     long_description="⚡ Deepchecks for Lightning ⚡",
     author="Kaushik Bokka",
     author_email="kaushik@lightning.ai",
     url="https://github.com/Lightning-AI/LAI-Deepchecks-Component",
-    packages=find_packages(exclude=["tests", "docs"]),
+    packages=find_packages(),
     keywords=["deeplearning", "deepchecks", "AI", "validation", "monitoring"],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     setup_requires=["wheel"],
-    install_requires=_load_requirements(os.path.dirname(__file__)),
+    install_requires=_load_requirements(),
 )
